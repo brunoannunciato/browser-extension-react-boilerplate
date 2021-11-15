de9e8b2f38073f045531f51e502d36269670a5a7
brunoannunciato <brunobannunciato@gmail.com>
2021-11-14 21:40:10 -0300

    :wrench: boilerplate mvp

---
+++ src/option-page/option.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.scss'
+
+const App = () => {
+  return (
+    <div>
+      Option page
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
